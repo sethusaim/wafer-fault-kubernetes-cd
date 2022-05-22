a98c349fe2b5a0d3ce67f0ae8e470570bd88e0d3
sethusaim <sethusaim@gmail.com>
2022-05-22 15:04:42 +0530

    finally made things work

--- deploy_app.sh
+++ deploy_app.sh
@@ -2,4 +2,8 @@
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 347460842118.dkr.ecr.us-east-1.amazonaws.com
 
-docker run -p 8080:8080 --name wafer_app 347460842118.dkr.ecr.us-east-1.amazonaws.com/$REPO_NAME:$DOCKERTAG
+if [ "$DOCKERTAG" == "1" ];then
+    docker run -p 8080:8080 --name wafer_app 347460842118.dkr.ecr.us-east-1.amazonaws.com/$REPO_NAME:$DOCKERTAG
+else
+    docker stop wafer_app && docker rm wafer_app && docker run -p 8080:8080 --name wafer_app 347460842118.dkr.ecr.us-east-1.amazonaws.com/$REPO_NAME:$DOCKERTAG
+fi
