07686a3ebb1357f99b03ec286460b34d2bd99f26
sethusaim <sethusaim@gmail.com>
2022-05-22 15:22:25 +0530

    fix

--- deploy_app.sh
+++ deploy_app.sh
@@ -6,7 +6,7 @@ echo $DOCKERTAG
 
 echo $REPO_NAME
 
-if [ "$DOCKERTAG" == "1" ];then
+if [ "$DOCKERTAG" == 1 ];then
     docker run -p 8080:8080 --name wafer_app 347460842118.dkr.ecr.us-east-1.amazonaws.com/$REPO_NAME:$DOCKERTAG
 else
     docker stop wafer_app && docker rm wafer_app && docker run -p 8080:8080 --name wafer_app 347460842118.dkr.ecr.us-east-1.amazonaws.com/$REPO_NAME:$DOCKERTAG
