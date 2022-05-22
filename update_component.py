325943eb218a65afccfeafd2aab626a12aea55e2
sethusaim <sethusaim@gmail.com>
2022-05-22 10:38:41 +0530

    changed update_component.py for tekton components

--- update_component.py
+++ update_component.py
@@ -21,16 +21,16 @@ def update_yaml():
     try:
         config = read_yaml()
 
-        old_image = config["implementation"]["container"]["image"]
+        old_image = config["spec"]["steps"][0]["image"]
 
         tagless_image = old_image.split(":")[0]
 
         new_image = tagless_image + ":" + str(new_tag)
 
-        config["implementation"]["container"]["image"] = new_image
+        config["spec"]["steps"][0]["image"] = new_image
 
         with open(file_name, "w") as fp:
-            yaml.safe_dump(config, fp)
+            yaml.safe_dump(config, fp, sort_keys=False)
 
     except Exception as e:
         raise e
